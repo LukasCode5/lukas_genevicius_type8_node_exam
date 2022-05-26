bb76eaff5bb2d482a5b6e0338c21dff68bcf1872
Lukasgit config --global user.email lukas@gmail.comgit add .git init -m init <you@example.com>
2022-05-26 08:29:55 +0300

    server works.

--- src/server.js
+++ src/server.js
@@ -1,1 +1,19 @@
-console.log('server.js works');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const { PORT } = require('./config');
+
+const app = express();
+
+// middleWare
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+// 404 route
+app.all('*', (req, res) => {
+  res.status(404).json({ error: 'Page not found' });
+});
+
+app.listen(PORT, () => console.log('Server online on port', PORT));
